b8aeb06db4a4bfb81f3f5fb75e73e0887eb98fce
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2025-03-12 13:26:22 -0600

    Agregar registro de blueprints para productos y órdenes en la aplicación

--- client/app/__init__.py
+++ client/app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .users import users_bp
+from .products import products_bp
+from .orders import orders_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(users_bp)
+    app.register_blueprint(products_bp)
+    app.register_blueprint(orders_bp)
     return app
