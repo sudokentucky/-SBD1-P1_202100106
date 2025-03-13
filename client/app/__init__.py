dfd939289f4cfcd64288c39d10b3a4be4e152746
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2025-03-13 16:39:54 -0600

    Agregar registro del blueprint de pagos en la aplicación Flask

--- client/app/__init__.py
+++ client/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .users import users_bp
 from .products import products_bp
 from .orders.routes import orders_bp
+from .payments import payments_bp
 
 
 
@@ -10,4 +11,5 @@ def create_app():
     app.register_blueprint(users_bp)
     app.register_blueprint(products_bp)
     app.register_blueprint(orders_bp)
+    app.register_blueprint(payments_bp)
     return app
